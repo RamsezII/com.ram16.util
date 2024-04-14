8076ca0134cfe11c9d8d5d6497216f883026b47c
ram16 <thomass.montaine@gmail.com>
2024-04-14 18:42:05 +0200

    signed

---
+++ Util/Math/Util.eulers.cs
@@ -0,0 +1,18 @@
+﻿public static partial class Util
+{
+    public static float SignedAngle(this float angle)
+    {
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -180)
+            angle += 360;
+        return angle;
+    }
+
+    public static float UnsignedAngle(this float angle)
+    {
+        if (angle < 0)
+            angle += 360;
+        return angle;
+    }
+}
