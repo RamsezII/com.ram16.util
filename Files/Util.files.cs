0987b684d3c2ff26ba2be75bd114b04d4d5184f3
ram16 <thomass.montaine@gmail.com>
2024-04-24 14:53:09 +0200

    home path

--- Files/Util.files.cs
+++ Files/Util.files.cs
@@ -3,6 +3,11 @@ using System.IO;
 
 public static partial class Util
 {
+    public static readonly string HOME_PATH = Path.Combine(Directory.GetCurrentDirectory(), "Home");
+    public static DirectoryInfo HOME_DIR => HOME_PATH.GetDir(true);
+
+    //----------------------------------------------------------------------------------------------------------
+
     public static long SecondsSinceLastWriteTime(this FileInfo file) => (DateTime.UtcNow - file.LastWriteTimeUtc).Ticks / TimeSpan.TicksPerSecond;
 
     public static DirectoryInfo GetDir(this string path, in bool force = true)
