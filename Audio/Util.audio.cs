d19fa2644a3a08045517cf5bc7534cbbe466019f
ram16 <thomass.montaine@gmail.com>
2024-05-04 03:22:55 +0200

    un peu d'audio

---
+++ Audio/Util.audio.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public static partial class Util
+{
+    public static bool SetVolume(this AudioMixer mixer, in string parameter, in float value) => mixer.SetFloat(parameter, value > 0 ? Mathf.Log10(value) * 20 : -80);
+    public static bool GetVolume(this AudioMixer mixer, in string parameter, out float value)
+    {
+        bool res = mixer.GetFloat(parameter, out value);
+        value = Mathf.Pow(10, value / 20);
+        return res;
+    }
+}
