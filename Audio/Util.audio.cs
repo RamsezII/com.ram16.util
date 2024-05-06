26f1d0b6edbcef9ecafd64337d590e94b76a0a62
ram16 <thomass.montaine@gmail.com>
2024-05-06 16:47:44 +0200

    utilaudio

--- Audio/Util.audio.cs
+++ Audio/Util.audio.cs
@@ -3,8 +3,8 @@ using UnityEngine.Audio;
 
 public static partial class Util
 {
-    public static bool SetVolume(this AudioMixer mixer, in string parameter, in float value) => mixer.SetFloat(parameter, value > 0 ? Mathf.Log10(value) * 20 : -80);
-    public static bool GetVolume(this AudioMixer mixer, in string parameter, out float value)
+    public static bool SetVolume_log(this AudioMixer mixer, in string parameter, in float value) => mixer.SetFloat(parameter, value > 0 ? Mathf.Log10(value) * 20 : -80);
+    public static bool GetVolume_log(this AudioMixer mixer, in string parameter, out float value)
     {
         bool res = mixer.GetFloat(parameter, out value);
         value = Mathf.Pow(10, value / 20);
