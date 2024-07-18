ecfcd574fb2cf652a0173364bdb6e56ee48b565c
ram16 <thomass.montaine@gmail.com>
2024-07-18 18:26:56 +0200

    colors adapt to editor theme

--- Util/Strings/Util.colored.cs
+++ Util/Strings/Util.colored.cs
@@ -2,10 +2,26 @@
 
 public static partial class Util
 {
+    static string sublogColor = "#BBBBBBBB";
+
+    //--------------------------------------------------------------------------------------------------------------
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void InitColors()
+    {
+        Debug.Log($"{nameof(UnityEditor.EditorGUIUtility.isProSkin)}: {UnityEditor.EditorGUIUtility.isProSkin}");
+        if (UnityEditor.EditorGUIUtility.isProSkin)
+            sublogColor = "#CCCCCCCC";
+        else
+            sublogColor = "#EAEAEAEA";
+    }
+
+    //--------------------------------------------------------------------------------------------------------------
+
     public static string SetColor(this string text, in Colors color) => $"<color={color}>{text}</color>";
     public static string SetColor(this string text, in string value) => $"<color={value}>{text}</color>";
     public static string ToSubLog(this object o) => ToSubLog(o.ToString());
-    public static string ToSubLog(this string text) => text.SetAttribute(TextB.italic).SetAttribute(TextB.color, "#BBBBBBBB");
+    public static string ToSubLog(this string text) => text.SetAttribute(TextB.italic).SetAttribute(TextB.color, sublogColor);
     public static string TipToLog(this string tip) => $"<i><color=#DDDDDD>tip:</color> {tip}</i>";
     public static void LogAsTip(this string tip) => Debug.Log(TipToLog(tip));
     public static string Message(this System.Exception e) => $"{e.GetType()} : \"{e.Message.TrimEnd('\n', '\r', '\t')}\"";
