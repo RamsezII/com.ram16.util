670e62471bb10e6f9ad9e9199068dd48063df446
ram16 <thomass.montaine@gmail.com>
2024-08-04 12:55:05 +0200

    nice

---
+++ Util/Other/Util_resources.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static partial class Util
+{
+    public static T LoadResourceByType<T>() where T : Object
+    {
+        return Resources.Load<T>(typeof(T).FullName);
+    }
+}
