2bf24607d035aafaca96dccf028861b56919defa
ram16 <thomass.montaine@gmail.com>
2024-08-15 13:26:00 +0200

    plus besoin de NetSocket

---
+++ Network/Util_net.TCP.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System;
+using UnityEngine;
+
+public static partial class Util_net
+{
+    public const ushort TCP_CHUNK = 1024;
+
+    //----------------------------------------------------------------------------------------------------------
+
+    public static void TryTCP_paragon(Action<TcpClient, BinaryWriter, BinaryReader> onCommunication, Action onError = null) => TryTCP(DOMAIN_3VE, PORT_PARAGON, onCommunication, onError);
+    public static void TryTCP(in string host, in ushort port, in Action<TcpClient, BinaryWriter, BinaryReader> onCommunication, in Action onError = null)
+    {
+        string endPoint = $"{{ {host}:{port} }}";
+
+        Debug.Log($"TCP connecting to: {endPoint}".ToSubLog());
+
+        try
+        {
+            double t1 = Util.TotalMilliseconds;
+            using TcpClient socket = new(host, port);
+
+            if (socket.Connected)
+            {
+                using NetworkStream stream = socket.GetStream();
+                using BinaryWriter writer = new(stream, Encoding.UTF8);
+                using BinaryReader reader = new(stream, Encoding.UTF8);
+
+                double t2 = Util.TotalMilliseconds - t1;
+                Debug.Log($"TCP connected to: {endPoint} ({t2.MillisecondsLog()})".ToSubLog());
+
+                t1 = Util.TotalMilliseconds;
+                onCommunication(socket, writer, reader);
+
+                t2 = Util.TotalMilliseconds - t1;
+                Debug.Log($"TCP closing: {endPoint} ({t2.MillisecondsLog()})".ToSubLog());
+
+                return;
+            }
+            else
+                Debug.LogWarning($"TCP connection failure {endPoint}");
+        }
+        catch (SocketException e)
+        {
+            Debug.LogWarning($"{nameof(SocketException)} {endPoint} \"{e.Message.TrimEnd('\n', '\r', '\t', '\0')}\"");
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+        onError?.Invoke();
+    }
+}
---
+++ Network/Util_net.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public static partial class Util_net
+{
+    public static readonly IPAddress
+        IP_3VE = IPAddress.Parse("141.94.223.114"),
+        IP_GOOGLE = IPAddress.Parse("8.8.8.8");
+
+    public const string
+        DOMAIN_3VE = "www.shitstorm.ovh";
+
+    public const ushort
+        PORT_ARMA = 40000,
+        PORT_ATHENA = 50000,
+        PORT_NUCLEOR = 60000,
+        PORT_PARAGON = 65000;
+
+    public static readonly IPEndPoint
+        end_ARMA = new(IP_3VE, PORT_ARMA),
+        end_ARMA_loop = new(IPAddress.Loopback, PORT_ARMA),
+        end_init = end_ARMA_loop;
+
+    static IPAddress localIP;
+
+    //----------------------------------------------------------------------------------------------------------
+
+    static Util_net()
+    {
+        localIP = null;
+    }
+
+    public static IPAddress GetLocalIP()
+    {
+        if (localIP == null)
+        {
+            using Socket socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Connect(new IPEndPoint(IP_GOOGLE, PORT_ARMA));
+            localIP = ((IPEndPoint)socket.LocalEndPoint).Address;
+        }
+        return localIP;
+    }
+
+    public static void LogIpConfig()
+    {
+        string hostname = Dns.GetHostName();
+        StringBuilder log = new($"host name: {hostname}\n");
+
+        foreach (IPAddress ip in Dns.GetHostEntry(hostname).AddressList)
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+                log.Append($"local ip: {ip}\n");
+
+        Debug.Log(log.ToString()[..^1]);
+    }
+}
