44068ed8dd4e94b1bd96138773a3d7aaa89b1913
ram16 <thomass.montaine@gmail.com>
2024-09-30 00:03:13 +0200

    Update Util.read.cs

--- Util/Strings/Util.read.cs
+++ Util/Strings/Util.read.cs
@@ -1,6 +1,8 @@
-﻿partial class Util
+﻿using UnityEngine;
+
+partial class Util
 {
-    public static bool TryReadWord(this string line, out string word, out string newline, in bool trim)
+    public static bool TryReadWord(this string line, out string word, out string newline, in bool trim = true)
     {
         if (trim)
             line = line.Trim();
@@ -24,5 +26,46 @@
             newline = newline.Trim();
 
         return true;
+    }
+
+    public static bool TryReadFloat(this string line, out float value, out string newline)
+    {
+        try
+        {
+            if (line.TryReadWord(out string word, out newline))
+                if (float.TryParse(word, out value))
+                    return true;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning($"{typeof(Util).FullName}.{nameof(TryReadFloat)}: \"{e.Message}\"");
+        }
+
+        value = 0;
+        newline = string.Empty;
+        return false;
+    }
+
+    public static bool TryReadVector3(this string line, out Vector3 value, out string newline)
+    {
+        value = Vector3.zero;
+
+        try
+        {
+            if (line.TryReadWord(out string word, out newline))
+                if (float.TryParse(word, out value.x))
+                    if (newline.TryReadWord(out word, out newline))
+                        if (float.TryParse(word, out value.y))
+                            if (newline.TryReadWord(out word, out newline))
+                                if (float.TryParse(word, out value.z))
+                                    return true;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning($"{typeof(Util).FullName}.{nameof(TryReadVector3)}: \"{e.Message}\"");
+        }
+
+        newline = string.Empty;
+        return false;
     }
 }
