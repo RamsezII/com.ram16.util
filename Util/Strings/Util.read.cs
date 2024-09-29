6983e629a670cf9bd9c2011cb28a2569b2616229
ram16 <thomass.montaine@gmail.com>
2024-09-29 23:55:16 +0200

    TryReadWord

---
+++ Util/Strings/Util.read.cs
@@ -0,0 +1,28 @@
+﻿partial class Util
+{
+    public static bool TryReadWord(this string line, out string word, out string newline, in bool trim)
+    {
+        if (trim)
+            line = line.Trim();
+
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            newline = string.Empty;
+            word = string.Empty;
+            return false;
+        }
+
+        int end;
+        for (end = 0; end < line.Length; end++)
+            if (char.IsWhiteSpace(line[end]))
+                break;
+
+        word = line[..end];
+        newline = line[end..];
+
+        if (trim)
+            newline = newline.Trim();
+
+        return true;
+    }
+}
