c1c5fcc3596abb58f59e47de36312bd52b02c5b3
ram16 <thomass.montaine@gmail.com>
2024-10-19 02:36:05 +0200

    ajouté Lerper

---
+++ Classes/Lerper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace _UTIL_
+{
+    public abstract class Lerper<T>
+    {
+        public float timeA, timeB;
+        public T valueA = default, valueB = default;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public void Update(in float nowTime, in float nextTime, in T value)
+        {
+            valueA = GetLerp(nowTime);
+            timeA = nowTime;
+            timeB = nextTime;
+            valueB = value;
+        }
+
+        public T GetLerp(in float time) => GetLerp_(Mathf.InverseLerp(timeA, timeB, time));
+        protected abstract T GetLerp_(in float lerp);
+    }
+}
