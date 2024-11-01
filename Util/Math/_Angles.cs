aa939ba1d0e63bde892c8fa2e2e9dea06dbfe322
ram16 <thomass.montaine@gmail.com>
2024-11-01 14:12:56 +0100

    angles

---
+++ Util/Math/_Angles.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public static partial class Util
+{
+    public static float SignedAngle(in Vector3 from, in Vector3 to, in Vector3 axis) => Vector3.SignedAngle(Vector3.ProjectOnPlane(from, axis), Vector3.ProjectOnPlane(to, axis), axis);
+
+    public static float SignedAngle(this float angle)
+    {
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -180)
+            angle += 360;
+        return angle;
+    }
+
+    public static float UnsignedAngle(this float angle)
+    {
+        if (angle < 0)
+            angle += 360;
+        return angle;
+    }
+
+}
