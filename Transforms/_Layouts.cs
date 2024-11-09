a6834864fd9f51f1f266db3fed70899fa68b3969
ram16 <thomass.montaine@gmail.com>
2024-11-09 02:12:24 +0100

    peu de changes

---
+++ Transforms/_Layouts.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+partial class Util
+{
+    public static void AutoSize(this LayoutGroup layoutGroup)
+    {
+        RectTransform rT = (RectTransform)layoutGroup.transform;
+        Vector2 sizeDelta = rT.sizeDelta;
+        sizeDelta.y = layoutGroup.preferredHeight;
+        rT.sizeDelta = sizeDelta;
+    }
+}
