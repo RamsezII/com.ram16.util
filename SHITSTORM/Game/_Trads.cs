34124f4c765dd2729eb40beca39c0a2c21f20bf6
ram16 <thomass.montaine@gmail.com>
2024-11-10 11:36:21 +0100

    update trads

---
+++ SHITSTORM/Game/_Trads.cs
@@ -0,0 +1,6 @@
+﻿using _UTIL_;
+
+partial class Util
+{
+    public static void SetTrads(this Traductable trad, in bool value) => trad.SetTrads(new() { english = value ? "Yes" : "No", french = value ? "Oui" : "Non", });
+}
