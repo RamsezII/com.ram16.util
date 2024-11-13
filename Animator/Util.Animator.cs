a55ccd57d0d5157abe740460be2d330e9900af5e
ram16 <thomass.montaine@gmail.com>
2024-11-13 02:47:46 +0100

    Update Util.Animator.cs

--- Animator/Util.Animator.cs
+++ Animator/Util.Animator.cs
@@ -6,6 +6,14 @@ public static partial class Util
 
     public static float GetNormlizedTimeClamped(this Animator animator, in int layerIndex = 0) => Mathf.Clamp01(GetNormlizedTime(animator, layerIndex));
 
+    public static float GetNormlizedTimeModulo(this Animator animator, in int layerIndex = 0)
+    {
+        float ntime = GetNormlizedTime(animator, layerIndex);
+        if (ntime > 1)
+            ntime %= 1;
+        return ntime;
+    }
+
     public static AnimatorStateInfo GetStateInfo(this Animator animator, in int layerIndex) => animator.IsInTransition(layerIndex) ? animator.GetNextAnimatorStateInfo(layerIndex) : animator.GetCurrentAnimatorStateInfo(layerIndex);
 
     public static Transform GetRootBone(this Animator animator)
