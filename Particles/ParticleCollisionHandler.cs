903eb22c1d56e17a0f51f973c4f64c47bd0caa68
ram16 <thomass.montaine@gmail.com>
2024-11-21 18:55:51 +0100

    particlecollisionhandler

---
+++ Particles/ParticleCollisionHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _UTIL_
+{
+    public class ParticleCollisionHandler : MonoBehaviour
+    {
+        public new ParticleSystem particleSystem;
+        public Action<GameObject, ParticleCollisionEvent> onParticleCollision;
+        readonly List<ParticleCollisionEvent> collisionEvents = new();
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Awake()
+        {
+            particleSystem = GetComponentInChildren<ParticleSystem>(true);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void OnParticleCollision(GameObject other)
+        {
+            if (onParticleCollision == null)
+                Debug.LogWarning($"{this}({transform.GetPath(true)}).{nameof(onParticleCollision)}({onParticleCollision.GetType().FullName}) not set", this);
+            else
+            {
+                int numCollisionEvents = ParticlePhysicsExtensions.GetCollisionEvents(particleSystem, other, collisionEvents);
+                for (int i = 0; i < numCollisionEvents; i++)
+                    onParticleCollision(other, collisionEvents[i]);
+            }
+        }
+    }
+}
