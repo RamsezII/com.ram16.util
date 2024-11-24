e06e3b84e70b508490469c02086885078a75a5f6
ram16 <thomass.montaine@gmail.com>
2024-11-24 03:48:30 +0100

    shockwave

---
+++ Curves/ShockwaveValues.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _UTIL_
+{
+    [Serializable]
+    public readonly struct ShockwaveValues
+    {
+        public readonly float force, lifetime, speed, treshold, offset;
+
+        //----------------------------------------------------------------------------------------------------------
+
+        public ShockwaveValues(in float force, in float lifetime, in float treshold, in float offset = 0)
+        {
+            this.force = force;
+            this.lifetime = lifetime;
+            speed = 1 / lifetime;
+            this.treshold = treshold;
+            this.offset = offset;
+        }
+
+        //----------------------------------------------------------------------------------------------------------
+
+        public readonly bool TryReadValue(float localTime, out float value)
+        {
+            localTime -= offset;
+            localTime *= speed;
+            if (localTime > 0 && localTime < 1)
+            {
+                value = force * treshold.EvaluateRecoilCurve(localTime);
+                return true;
+            }
+            else
+            {
+                value = 0;
+                return false;
+            }
+        }
+    }
+}
---
+++ Curves/Util_curves.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+partial class Util
+{
+    public static float EvaluateRecoilCurve(this in float treshold, in float time)
+    {
+        float lerp;
+        if (time <= treshold)
+            lerp = Mathf.SmoothStep(-1, 1, Mathf.InverseLerp(-treshold, treshold, time));
+        else
+            lerp = Mathf.SmoothStep(0, 1, Mathf.InverseLerp(1, treshold, time));
+        return lerp;
+    }
+}
