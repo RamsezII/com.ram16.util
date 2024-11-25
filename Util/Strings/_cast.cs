5fc220fe6daa3304dea8c9e33439d8128f5cdc9e
ram16 <thomass.montaine@gmail.com>
2024-11-25 22:26:58 +0100

    string cast

---
+++ Util/Strings/_cast.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+partial class Util
+{
+    public static bool TryCastEndOfLine(this string input, out int output, out string match, out int length)
+    {
+        Match regex = Regex.Match(input, @"\d+$");
+        if (regex.Success)
+        {
+            match = regex.Value;
+            length = regex.Length;
+            output = int.Parse(match);
+            return true;
+        }
+        else
+        {
+            match = null;
+            length = 0;
+            output = 0;
+            return false;
+        }
+    }
+}
