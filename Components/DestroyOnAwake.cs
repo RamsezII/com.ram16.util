4a71cc440e5584370a7ec14ce170eb3526b86260
ram16 <thomass.montaine@gmail.com>
2024-11-29 21:11:35 +0100

    util

---
+++ Components/DestroyOnAwake.cs
@@ -0,0 +1,18 @@
+﻿using _UTIL_;
+using UnityEngine;
+
+namespace _UTIL_
+{
+    internal class DestroyOnAwake : MonoBehaviour
+    {
+        private void Awake() => Destroy(gameObject);
+    }
+}
+
+#if UNITY_EDITOR
+partial class Util
+{
+    [UnityEditor.MenuItem("CONTEXT/" + nameof(Transform) + "/" + nameof(AddDestroyOnAwake))]
+    static void AddDestroyOnAwake(UnityEditor.MenuCommand command) => ((Transform)command.context).gameObject.AddComponent<DestroyOnAwake>();
+}
+#endif
