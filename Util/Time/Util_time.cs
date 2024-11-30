eccff316f00610cc3ec00f8228fc95ec8a6ffcfc
ram16 <thomass.montaine@gmail.com>
2024-11-30 17:42:36 +0100

    util_time

---
+++ Util/Time/Util_time.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+partial class Util
+{
+    public static float AutoTime() => Time.inFixedTimeStep ? Time.fixedTime : Time.time;
+    public static float AutoTimeUnscaled() => Time.inFixedTimeStep ? Time.fixedUnscaledTime : Time.unscaledTime;
+}
