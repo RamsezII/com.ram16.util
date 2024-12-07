1967be9479e4ee77db2fcc0a0898a6950d9f2928
ram16 <thomass.montaine@gmail.com>
2024-12-07 04:23:00 +0100

    léger

---
+++ Particles/ParticleOrphenizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace _UTIL_
+{
+    internal class ParticleOrphenizer : MonoBehaviour
+    {
+        [SerializeField] new ParticleSystem particleSystem;
+        [SerializeField] float delay = 1;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void OnDestroy()
+        {
+            if (particleSystem != null)
+            {
+                particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+                particleSystem.transform.SetParent(null, true);
+                Destroy(particleSystem.gameObject, delay);
+            }
+        }
+    }
+}
