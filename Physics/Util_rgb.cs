abf14a97265a5559ee6b64628d76d0ec1245bb43
ram16 <thomass.montaine@gmail.com>
2024-12-25 03:21:59 +0100

    shnoyce

---
+++ Physics/Util_rgb.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+partial class Util
+{
+    public static Vector3 TransformPoint(this Rigidbody rigidbody, in Vector3 localPoint) => rigidbody.position + rigidbody.rotation * localPoint;
+    public static Vector3 InverseTransformPoint(this Rigidbody rigidbody, in Vector3 worldPoint) => Quaternion.Inverse(rigidbody.rotation) * (worldPoint - rigidbody.position);
+
+    public static Vector3 TransformDirection(this Rigidbody rigidbody, in Vector3 localDirection) => rigidbody.rotation * localDirection;
+    public static Vector3 InverseTransformDirection(this Rigidbody rigidbody, in Vector3 worldDirection) => Quaternion.Inverse(rigidbody.rotation) * worldDirection;
+}
--- Util/Strings/Util.strings.cs
+++ Util/Strings/Util.strings.cs
@@ -2,27 +2,10 @@
 
 public static partial class Util
 {
+    public static string ReplaceIfEmpty(this string text, in string replace) => string.IsNullOrEmpty(text) ? replace : text;
+    public static string ReplaceIfNullOrWhiteSpace(this string text, in string replace) => string.IsNullOrWhiteSpace(text) ? replace : text;
+
     public static string Join(this IEnumerable<string> strings, in string separator, in string prefix = null, in string suffix = null) => prefix + string.Join(separator, strings) + suffix;
-    public static string Join(this string separator, params string[] strings) => string.Join(separator, strings);
-
-    public static string PullValue(ref string value)
-    {
-        string output = value;
-        value = null;
-        return output;
-    }
-
-    public static bool TryPullValue(ref string value, out string output)
-    {
-        if (value == null)
-        {
-            output = null;
-            return false;
-        }
-        output = value;
-        value = null;
-        return true;
-    }
 
     public static bool IsNull(this string text, in bool whitSpaces = true)
     {
@@ -30,29 +13,6 @@ public static partial class Util
             return string.IsNullOrWhiteSpace(text);
         else
             return string.IsNullOrEmpty(text);
-    }
-
-    public static bool TryReadUrlParameter(this string url, in string parameter, out string value)
-    {
-        int start = url.IndexOf(parameter);
-        if (start > 0)
-        {
-            int endOfValue = url.IndexOf('&', start);
-            start += parameter.Length + 1;
-
-            if (endOfValue > start)
-            {
-                value = url[start..endOfValue];
-                return true;
-            }
-            else
-            {
-                value = string.Empty;
-                return true;
-            }
-        }
-        value = string.Empty;
-        return false;
     }
 
     public static string SetAttribute(this string text, in TextB attribute)
@@ -65,13 +25,5 @@ public static partial class Util
     {
         string attr = Util_richtext.rtextAttr[(int)attribute];
         return $"<{attr}={value}>{text}</{attr}>";
-    }
-
-    public static string SetAttributes(this string text, in TextF attributes)
-    {
-        for (TextB tb = 0; tb < TextB._last_; ++tb)
-            if (attributes.HasFlag((TextF)(1 << (int)tb)))
-                text = text.SetAttribute(tb);
-        return text;
     }
 }
