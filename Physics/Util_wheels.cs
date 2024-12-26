ef4035ff7181c87271e94770511cbcaa05a32188
ram16 <thomass.montaine@gmail.com>
2024-12-26 20:50:47 +0100

    kek bails

---
+++ Physics/Util_wheels.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+partial class Util
+{
+    public static float MPS(this WheelCollider wheel) => Mathf.PI * wheel.rpm * wheel.radius / 30;
+    public static float ToMPS(this in float rpm, in float radius) => Mathf.PI * rpm * radius * 30;
+    public static float ToRPM(this in float mps, in float radius) => mps / (Mathf.PI * radius * 30);
+}
