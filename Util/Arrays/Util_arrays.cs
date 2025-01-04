74dd3cfdf3fa95b137f48f502866aad1d5cabe89
ram16 <thomass.montaine@gmail.com>
2025-01-04 16:28:24 +0100

    arraysegments

---
+++ Util/Arrays/Util_arrays.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+partial class Util
+{
+    public static IList<T> SegmentF<T>(this T[] array, in Range range) => range.End.IsFromEnd ? SegmentFE(array, range) : SegmentFS(array, range);
+    public static IList<T> SegmentFS<T>(this T[] array, in Range range) => new ArraySegment<T>(array, range.Start.Value, range.End.Value - range.Start.Value);
+    public static IList<T> SegmentFE<T>(this T[] array, in Range range) => new ArraySegment<T>(array, range.Start.Value, array.Length - range.End.Value - range.Start.Value);
+}
