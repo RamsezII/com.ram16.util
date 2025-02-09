1597af14dc1e0b0fe7b7897debba1d696374a0e2
ram16 <thomass.montaine@gmail.com>
2025-02-09 18:36:56 +0100

    Duons

---
+++ Classes/DuonGod.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _UTIL_
+{
+    public interface IDuon
+    {
+        byte DuonID { get; set; }
+        void DestroyDuon();
+    }
+
+    public class DuonGod
+    {
+        public readonly IDuon[] duons;
+        [SerializeField] byte currentID;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public DuonGod(in byte count)
+        {
+            duons = new IDuon[count];
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public void ReserveDuon(in IDuon duon)
+        {
+            currentID = (byte)((currentID + 1) % duons.Length);
+            if (duons[currentID] != null)
+                duons[currentID].DestroyDuon();
+            duons[currentID] = duon;
+            duon.DuonID = currentID;
+        }
+    }
+}
