25e6aa4eebdf20f11b58b637a70afff8de3b482d
ram16 <thomass.montaine@gmail.com>
2025-03-07 17:37:26 +0100

    Update Util_bytes.cs

--- Streams/Util_bytes.cs
+++ Streams/Util_bytes.cs
@@ -1,39 +1,13 @@
-﻿using System.IO;
-using System.Text;
+﻿using System.Text;
 
 public static partial class Util
 {
-    public static string LogBytes(this BinaryWriter writer) => LogBytes(writer.BaseStream);
-    public static string LogBytes(this BinaryReader reader) => LogBytes(reader.BaseStream);
-    public static string LogBytes(this Stream stream)
+    public static string LogBytes(this byte[] buffer, in int start, in int end)
     {
-        StringBuilder log = new();
-
-        lock (stream)
-        {
-            int pos = (int)stream.Position;
-            stream.Position = 0;
-
-            for (int i = 0; i < stream.Length; i++)
-            {
-                if (i == pos)
-                    log.Append("| ");
-                log.Append($"[{i}]:{stream.ReadByte()} ");
-            }
-            stream.Position = pos;
-        }
-
-        return log.ToString();
-    }
-
-    public static string LogBytes(this byte[] buffer)
-    {
-        StringBuilder log = new();
-
+        StringBuilder log = new($"{end - start} bytes: ");
         lock (buffer)
-            for (int i = 0; i < buffer.Length; i++)
-                log.Append($"[{i}]:{buffer[i]} ");
-
+            for (int i = start; i < end; i++)
+                log.Append($"| {buffer[i],3} ");
         return log.ToString();
     }
 }
