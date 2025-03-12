1f97772a54555b7eab6231c1427f5969a6fe940c
ram16 <thomass.montaine@gmail.com>
2025-03-12 03:16:30 +0100

    nouveaux chemins

--- Files/Util.files.cs
+++ Files/Util.files.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public static partial class Util
 {
-    public static readonly string
-        app_path = Directory.GetParent(Application.dataPath).FullName,
-        home_path = Path.Combine(app_path, "Home");
-
-    public static DirectoryInfo HOME_DIR => home_path.ForceDir();
-    public static DirectoryInfo STREAM_DIR => Application.streamingAssetsPath.ForceDir();
+    public static DirectoryInfo GetSreamingAssetsDir() => Application.streamingAssetsPath.ForceDir();
 
     //----------------------------------------------------------------------------------------------------------
 
+    public static bool Equals_path(this string path1, string path2) => Path.GetFullPath(path1).Equals(Path.GetFullPath(path2), StringComparison.OrdinalIgnoreCase);
+
     public static long SecondsSinceLastWriteTime(this FileInfo file) => (DateTime.UtcNow - file.LastWriteTimeUtc).Ticks / TimeSpan.TicksPerSecond;
 
-    public static DirectoryInfo ForceDir(this string path)
+    public static DirectoryInfo ForceDir(this string path, in bool force = true)
     {
         DirectoryInfo dir = new(path);
-        if (!dir.Exists)
-        {
-            dir.Create();
-            Debug.Log($"Created directory at: \"{dir.FullName}\"".ToSubLog());
-        }
+        if (force)
+            if (!dir.Exists)
+            {
+                dir.Create();
+                Debug.Log($"Created directory at: \"{dir.FullName}\"".ToSubLog());
+            }
         return dir;
     }
 
