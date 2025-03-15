5d9fee3665120e4c8ca5abf75dfb3238db1e1f0a
ram16 <thomass.montaine@gmail.com>
2025-03-15 18:15:07 +0100

    Update Util.files.cs

--- Files/Util.files.cs
+++ Files/Util.files.cs
@@ -10,21 +10,19 @@ public static partial class Util
 
     public static bool Equals_path(this string path1, string path2) => Path.GetFullPath(path1).Equals(Path.GetFullPath(path2), StringComparison.OrdinalIgnoreCase);
 
-    public static long SecondsSinceLastWriteTime(this FileInfo file) => (DateTime.UtcNow - file.LastWriteTimeUtc).Ticks / TimeSpan.TicksPerSecond;
-
-    public static DirectoryInfo ForceDir(this string path, in bool force = true)
+    public static DirectoryInfo ForceDir(this string path)
     {
         DirectoryInfo dir = new(path);
-        if (force)
-            if (!dir.Exists)
-            {
-                dir.Create();
-                Debug.Log($"Created directory at: \"{dir.FullName}\"".ToSubLog());
-            }
+        if (!dir.Exists)
+        {
+            dir.Create();
+            Debug.Log($"Created directory: \"{dir.FullName}\"".ToSubLog());
+        }
         return dir;
     }
 
     [Obsolete] public static string TypeToExtension_OLD(this Type type) => type.FullName.Replace(".", string.Empty).Replace('+', '_');
-    public static string TypeToExtension(this Type type) => "." + type.FullName.Replace(".", string.Empty).Replace('+', '_');
+    public static string TypeToFileName(this Type type) => type.FullName.Replace(".", string.Empty).Replace('+', '_');
+    public static string TypeToExtension(this Type type) => "." + TypeToFileName(type);
     public static string TypeToPath(this Type type) => type.FullName.Replace('.', Path.PathSeparator).Replace('+', '_');
 }
