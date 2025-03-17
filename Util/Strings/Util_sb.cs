ee02da8d2d506d2a44e5422ddb70e4b7a434a740
ram16 <thomass.montaine@gmail.com>
2025-03-17 15:26:37 +0100

    yes

---
+++ Util/Strings/Util_sb.cs
@@ -0,0 +1,8 @@
+﻿using System.Text;
+using UnityEngine;
+
+partial class Util
+{
+    public static string TroncatedForLog(this StringBuilder sb) => sb == null || sb.Length == 0 ? string.Empty : sb.ToString().TrimEnd('\n', '\r');
+    public static void Log(this StringBuilder sb, in Object o = null) => Debug.Log(TroncatedForLog(sb), o);
+}
