a168b2f4253f6c09c5033108f9cdfbe816bb05d6
ram16 <thomass.montaine@gmail.com>
2025-03-26 00:04:18 +0100

    Update StdText.cs

--- Streams/StdText.cs
+++ Streams/StdText.cs
@@ -2,39 +2,14 @@
 using System.Text;
 using UnityEngine;
 
+partial class Util
+{
+    public static float ReadFloat(this string[] lines, ref int read_i) => float.Parse(lines[read_i++]);
+    public static Vector3 ReadVector3(this string[] lines, ref int read_i) => new(ReadFloat(lines, ref read_i), ReadFloat(lines, ref read_i), ReadFloat(lines, ref read_i));
+}
+
 namespace _UTIL_
 {
-    public class StdReaderText : StreamReader
-    {
-        public StdReaderText(in string path) : base(path, Encoding.UTF8)
-        {
-
-        }
-
-        //----------------------------------------------------------------------------------------------------------
-
-        public bool ReadBool()
-        {
-            string read = ReadLine();
-            return read == "1";
-        }
-
-        public int ReadInt()
-        {
-            string read = ReadLine();
-            return int.Parse(read);
-        }
-
-        public float ReadFloat()
-        {
-            string read = ReadLine();
-            return float.Parse(read);
-        }
-
-        public Vector2 ReadVector2() => new(ReadFloat(), ReadFloat());
-        public Vector3 ReadVector3() => new(ReadFloat(), ReadFloat(), ReadFloat());
-    }
-
     public class StdWriterText : StreamWriter
     {
         public StdWriterText(in string path, in bool append = false) : base(path, append, Encoding.UTF8)
@@ -44,8 +19,6 @@ namespace _UTIL_
 
         //----------------------------------------------------------------------------------------------------------
 
-        public void WriteBool(in bool value) => WriteLine(value ? "1" : "0");
-        public void WriteVector2(in Vector2 value) => WriteLine($"{value.x}\n{value.y}");
         public void WriteVector3(in Vector3 value) => WriteLine($"{value.x}\n{value.y}\n{value.z}");
     }
 }
