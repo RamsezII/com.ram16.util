678705bffabf7848d201c2de24ae6e906195e606
ram16 <thomass.montaine@gmail.com>
2025-03-27 23:22:21 +0100

    Update Util_arrays.cs

--- Util/Arrays/Util_arrays.cs
+++ Util/Arrays/Util_arrays.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 
 partial class Util
 {
-    public static IList<T> SegmentF<T>(this T[] array, in Range range) => range.End.IsFromEnd ? SegmentFE(array, range) : SegmentFS(array, range);
-    public static IList<T> SegmentFS<T>(this T[] array, in Range range) => new ArraySegment<T>(array, range.Start.Value, range.End.Value - range.Start.Value);
-    public static IList<T> SegmentFE<T>(this T[] array, in Range range) => new ArraySegment<T>(array, range.Start.Value, array.Length - range.End.Value - range.Start.Value);
     public static byte[] CopyBytes(this BinaryWriter writer) => writer.GetBuffer()[..(int)writer.BaseStream.Position];
 }
