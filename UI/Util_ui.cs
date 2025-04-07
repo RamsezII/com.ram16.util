8e148482b3071083935e1e2df0621ca075c1a30b
ram16 <thomass.montaine@gmail.com>
2025-04-07 17:38:37 +0200

    yes

---
+++ UI/Util_ui.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+partial class Util
+{
+    public static bool IsSelectedByEventSystem(this GameObject go)
+    {
+        if (EventSystem.current == null)
+            return false;
+        if (EventSystem.current.currentSelectedGameObject == null)
+            return false;
+        return EventSystem.current.currentSelectedGameObject == go;
+    }
+}
