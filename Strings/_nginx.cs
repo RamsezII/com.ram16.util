4f51b8998558717e3ac4cb279666ebd9a0053c4f
ram16 <thomass.montaine@gmail.com>
2025-04-08 16:33:44 +0200

    Update _nginx.cs

--- Strings/_nginx.cs
+++ Strings/_nginx.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 partial class Util
 {
-    public static bool TryExtractIndex_FromNGinxText(this string text, out NGinxIndex index, in Action<string> onError = null)
+    public static bool TryExtractIndex_FromNGinxText(this string text, out NGinxIndex index, out string error)
     {
         string json = $"{{\"{nameof(NGinxIndex.entries)}\":{text}}}";
-        return TryExtractIndex_FromNGinxJSon(json, out index, onError);
+        return TryExtractIndex_FromNGinxJSon(json, out index, out error);
     }
 
-    public static bool TryExtractIndex_FromNGinxJSon(this string json, out NGinxIndex index, in Action<string> onError = null)
+    public static bool TryExtractIndex_FromNGinxJSon(this string json, out NGinxIndex index, out string error)
     {
         try
         {
             index = JsonUtility.FromJson<NGinxIndex>(json);
+            error = null;
             return true;
         }
         catch (Exception e)
         {
-            onError?.Invoke(e.TrimMessage());
+            error = $"Failed to parse index: {e.TrimMessage()}";
             Debug.LogException(e);
             index = null;
             return false;
@@ -32,13 +33,22 @@ namespace _UTIL_
     [Serializable]
     public class NGinxIndex
     {
+        public enum EntryTypes : byte
+        {
+            unknown,
+            file,
+            directory,
+            symlink,
+        }
+
         [Serializable]
         public class Entry
         {
-            public string name, type;
-            [SerializeField] string mtime;
+            public string name;
+            [SerializeField] string type, mtime;
             public int size;
             public DateTime ToDateTime => DateTime.TryParse(mtime, out DateTime date) ? date : DateTime.MinValue;
+            public EntryTypes GetEntryType => Enum.TryParse(type, true, out EntryTypes entryType) ? entryType : EntryTypes.unknown;
         }
 
         public Entry[] entries;
