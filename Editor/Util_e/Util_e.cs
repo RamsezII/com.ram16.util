1eea8955488a55834bd9ab66aaaaec344a76183a
ram16 <thomass.montaine@gmail.com>
2025-04-12 16:52:11 +0200

    Add Util_e editor script and associated meta files

---
+++ Editor/Util_e/Util_e.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace _UTIL_e
+{
+    internal static partial class Util_e
+    {
+        [MenuItem("Assets/"+nameof(_EDITOR_)+"/"+nameof(_TestBalls))]
+        static void _TestBalls()
+        {
+            Debug.Log("balls :)");
+        }
+    }
+}
