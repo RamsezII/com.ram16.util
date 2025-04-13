38f7685247c5fb3967ab540f96fe074499fb83c7
ram16 <thomass.montaine@gmail.com>
2025-04-13 14:47:57 +0200

    yess

--- Runtime/Strings/Util_sb.cs
+++ Runtime/Strings/Util_sb.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 partial class Util
 {
-    public static string TroncatedForLog(this StringBuilder sb) => sb == null || sb.Length == 0 ? string.Empty : sb.ToString().TrimEnd('\n', '\r');
+    public static string TrimmedForLog(this StringBuilder sb) => sb == null || sb.Length == 0 ? string.Empty : sb.ToString().TrimEnd('\n', '\r');
+    public static string TroncatedForLog(this StringBuilder sb) => sb == null || sb.Length == 0 ? string.Empty : sb.ToString()[..^1];
     public static void Log(this StringBuilder sb, in Object o = null) => Debug.Log(TroncatedForLog(sb), o);
     public static void LogAndClear(this StringBuilder sb, in Object o = null)
     {
