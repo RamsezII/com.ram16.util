a21bc289135f22fab316f4c1a43cab4837cb767b
ram16 <thomass.montaine@gmail.com>
2025-04-22 17:37:01 +0200

    continuer editor

---
+++ Runtime/Delegates/Util_actions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+partial class Util
+{
+    public static void AddAction(ref Action instance, in Action to_add)
+    {
+        instance -= to_add;
+        instance += to_add;
+    }
+}
