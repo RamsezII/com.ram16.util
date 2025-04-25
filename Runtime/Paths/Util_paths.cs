12b24563f4665d32c7827e8ff7eab123840b3116
ram16 <thomass.montaine@gmail.com>
2025-04-25 13:36:54 +0200

    continuer

--- Runtime/Paths/Util_paths.cs
+++ Runtime/Paths/Util_paths.cs
@@ -1,4 +1,25 @@
-﻿partial class Util
+﻿using System;
+using _UTIL_;
+
+namespace _UTIL_
 {
+    public enum FS_Types : byte
+    {
+        File,
+        Dir,
+    }
+
+    [Flags]
+    public enum FS_TYPES : byte
+    {
+        FILE = 1 << FS_Types.File,
+        DIRECTORY = 1 << FS_Types.Dir,
+        BOTH = FILE | DIRECTORY,
+    }
+}
+
+partial class Util
+{
+    public static bool HasFlags_any(this FS_TYPES mask, FS_TYPES flags) => (mask & flags) != 0;
     public static string ToLinuxPath(this string path) => path.Replace('\\', '/');
 }
