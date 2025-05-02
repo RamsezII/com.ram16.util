564773fb3e821b33fb01938db4df73d7404bdcd0
ram16 <thomass.montaine@gmail.com>
2025-05-02 18:34:47 +0200

    callbacks

---
+++ Runtime/Components/MonoBehaviourCallbacks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace _UTIL_
+{
+    public sealed class MonoBehaviourCallbacks : MonoBehaviour
+    {
+        public Action on_awake, on_start, on_enable, on_disable, on_destroy;
+        public Action<bool> on_toggle;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Awake()
+        {
+            on_awake?.Invoke();
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Start()
+        {
+            on_start?.Invoke();
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void OnEnable()
+        {
+            on_enable?.Invoke();
+            on_toggle?.Invoke(true);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void OnDisable()
+        {
+            on_disable?.Invoke();
+            on_toggle?.Invoke(false);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void OnDestroy()
+        {
+            on_destroy?.Invoke();
+        }
+    }
+}
