717af43f9c625ade48684fdfcab7cb4f84597e1f
ram16 <thomass.montaine@gmail.com>
2025-05-04 01:19:31 +0200

    vers EventPropagator

--- Runtime/Classes/EventPropagator.cs
+++ Runtime/Classes/EventPropagator.cs
@@ -4,7 +4,70 @@ using UnityEngine;
 
 namespace _UTIL_
 {
-    public class ListenersGroup<T> : IDisposable
+    public class EventPropagator : IDisposable
+    {
+        public interface IUser
+        {
+            public bool Disposed { get; }
+        }
+
+        readonly List<(Action action, object user)> listeners = new();
+        bool disposed;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public void AddListener(in Action action, in object user)
+        {
+            listeners.Add((action, user));
+        }
+
+        public void RemoveListener_action(in Action action)
+        {
+            for (int i = 0; i < listeners.Count; i++)
+                if (listeners[i].action == action)
+                {
+                    listeners.RemoveAt(i);
+                    break;
+                }
+        }
+
+        public void RemoveListener_user(in object user)
+        {
+            for (int i = 0; i < listeners.Count; i++)
+                if (listeners[i].user == user)
+                {
+                    listeners.RemoveAt(i);
+                    break;
+                }
+        }
+
+        public void NotifyListeners()
+        {
+            for (int i = 0; i < listeners.Count; ++i)
+            {
+                (Action action, object user) = listeners[i];
+                if (user == null || user is IUser iuser && iuser.Disposed)
+                    listeners.RemoveAt(i--);
+                else if (action == null)
+                    Debug.LogWarning($"null action in {GetType().FullName}");
+                else
+                    action();
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public void Dispose()
+        {
+            if (disposed)
+                return;
+            disposed = true;
+
+            listeners.Clear();
+        }
+    }
+
+    public class EventPropagator<T> : IDisposable
     {
         public interface IUser
         {
