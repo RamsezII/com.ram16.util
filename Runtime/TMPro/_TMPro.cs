693bd50c2d8c70df23f44db9eeef0e1c7db7a4cc
ram16 <thomass.montaine@gmail.com>
2025-05-08 20:39:23 +0200

    Update _TMPro.cs

--- Runtime/TMPro/_TMPro.cs
+++ Runtime/TMPro/_TMPro.cs
@@ -6,5 +6,5 @@ partial class Util
     public static void AutoSize(this TextMeshProUGUI tmp) => tmp.rectTransform.sizeDelta = new Vector2(tmp.preferredWidth, tmp.preferredHeight);
 
     public static TMP_Dropdown.OptionData CurrentData(this TMP_Dropdown dropdown) => dropdown.options[dropdown.value];
-    public static string CurrentText(this TMP_Dropdown dropdown) => CurrentData(dropdown).text;
+    public static string GetSelectedValue(this TMP_Dropdown dropdown) => CurrentData(dropdown).text;
 }
