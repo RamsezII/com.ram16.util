10928c021ddf558ecbe27227dc0f1c7fcecbd563
ram16 <thomass.montaine@gmail.com>
2025-05-10 15:35:26 +0200

    Update _reflection.cs

--- Runtime/Reflection/_reflection.cs
+++ Runtime/Reflection/_reflection.cs
@@ -1,45 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Reflection;
 using System;
-using UnityEngine;
 
 partial class Util
 {
-    public static IEnumerable<Type> EGetAllDerivedTypes<T>() where T : class
+    public static IEnumerable<Type> EGetAllDerivedTypes<T>(bool include_abstracts = false) where T : class
     {
         Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
         for (int i = 0; i < assemblies.Length; i++)
         {
             Assembly assembly = assemblies[i];
             foreach (Type type in assembly.GetTypes())
-                if (type.IsSubclassOf(typeof(T)) && !type.IsAbstract)
+                if (type.IsSubclassOf(typeof(T)) && (include_abstracts || !type.IsAbstract))
                     yield return type;
         }
-    }
-
-    public static List<Type> GetAllDerivedTypes<T>() where T : class
-    {
-        List<Type> derivedTypes = new();
-        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
-        for (int i = 0; i < assemblies.Length; i++)
-        {
-            Assembly assembly = assemblies[i];
-            try
-            {
-                foreach (Type type in assembly.GetTypes())
-                    if (type.IsSubclassOf(typeof(T)) && !type.IsAbstract)
-                        derivedTypes.Add(type);
-            }
-            catch (ReflectionTypeLoadException e)
-            {
-                // Gère les exceptions si certaines assemblées ne peuvent être chargées.
-                Debug.LogWarning($"ReflectionTypeLoadException: {e}");
-            }
-        }
-
-        return derivedTypes;
     }
 
     static bool CheckType(this Type type) => type != null && !type.IsAbstract;
