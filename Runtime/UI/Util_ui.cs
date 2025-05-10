9c1740a43c418c2a368881b6db0ab0ad72c264b3
ram16 <thomass.montaine@gmail.com>
2025-05-10 14:35:08 +0200

    Update Util_ui.cs

--- Runtime/UI/Util_ui.cs
+++ Runtime/UI/Util_ui.cs
@@ -3,6 +3,10 @@ using UnityEngine.EventSystems;
 
 partial class Util
 {
+    static readonly Vector3[] rt_corners = new Vector3[4];
+
+    //--------------------------------------------------------------------------------------------------------------
+
     public static bool IsSelectedByEventSystem(this GameObject go)
     {
         if (EventSystem.current == null)
@@ -10,5 +14,20 @@ partial class Util
         if (EventSystem.current.currentSelectedGameObject == null)
             return false;
         return EventSystem.current.currentSelectedGameObject == go;
+    }
+
+    public static (Vector2 min, Vector2 max) GetWorldCorners(this RectTransform rT)
+    {
+        lock (rt_corners)
+        {
+            rT.GetWorldCorners(rt_corners);
+            return (rt_corners[0], rt_corners[2]);
+        }
+    }
+
+    public static Vector2 GetWorldSize(this RectTransform rT)
+    {
+        (Vector2 min, Vector2 max) = rT.GetWorldCorners();
+        return max - min;
     }
 }
