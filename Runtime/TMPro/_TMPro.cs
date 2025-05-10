09010e6ef94c259c3172c355f4578400f8dd9fa8
ram16 <thomass.montaine@gmail.com>
2025-05-10 13:38:25 +0200

    Update _TMPro.cs

--- Runtime/TMPro/_TMPro.cs
+++ Runtime/TMPro/_TMPro.cs
@@ -7,4 +7,26 @@ partial class Util
 
     public static TMP_Dropdown.OptionData CurrentData(this TMP_Dropdown dropdown) => dropdown.options[dropdown.value];
     public static string GetSelectedValue(this TMP_Dropdown dropdown) => CurrentData(dropdown).text;
+
+    public static bool TryCurrentData(this TMP_Dropdown dropdown, out TMP_Dropdown.OptionData output)
+    {
+        if (dropdown.options.Count == 0)
+        {
+            output = null;
+            return false;
+        }
+        output = dropdown.options[dropdown.value];
+        return true;
+    }
+
+    public static bool TryGetSelectedValue(this TMP_Dropdown dropdown, out string output)
+    {
+        if (TryCurrentData(dropdown, out var data))
+        {
+            output = data.text;
+            return true;
+        }
+        output = null;
+        return false;
+    }
 }
