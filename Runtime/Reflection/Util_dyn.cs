ba9cf612f2ac144b4d9a37f10b9cd620d1ea709a
ram16 <thomass.montaine@gmail.com>
2025-05-15 19:03:15 +0200

    modify anonymous

---
+++ Runtime/Reflection/Util_dyn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+partial class Util
+{
+    public static object ModifyAnonymous<T>(this T target, string property, object value)
+    {
+        Type type = target.GetType();
+        PropertyInfo[] props = type.GetProperties();
+        object[] values = props.Select(p => p.Name.Equals(property, StringComparison.Ordinal) ? value : p.GetValue(target)).ToArray();
+
+        return type.GetConstructors().First().Invoke(values);
+    }
+
+    public static object GetProp(this object target, string property_name)
+    {
+        Type type = target.GetType();
+        PropertyInfo prop = type.GetProperty(property_name);
+        return prop.GetValue(target);
+    }
+}
